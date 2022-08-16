c78d5f083706b0a32a7d84c20c67483111f34dcf
elaniacs <carensousaesilva@gmail.com>
2022-08-16 11:09:45 -0300

    add dictionary and extracting optional with force unwrap

--- egg-timer/egg-timer/ViewController.swift
+++ egg-timer/egg-timer/ViewController.swift
@@ -9,10 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    let eggTimes = ["Soft": 5, "Medium": 7, "Hard": 12]
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        
+        let hardness = sender.currentTitle!
+        let result = eggTimes[hardness]!
+        
+        print(result)
+        
     }
+    
+    
+    
+    
 
 
 }
