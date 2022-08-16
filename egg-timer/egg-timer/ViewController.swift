ef96bb2141e4c1689479803adf49ebd8337d2204
elaniacs <carensousaesilva@gmail.com>
2022-08-16 16:07:26 -0300

    add timer

--- egg-timer/egg-timer/ViewController.swift
+++ egg-timer/egg-timer/ViewController.swift
@@ -9,21 +9,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    let eggTimes = ["Soft": 5, "Medium": 7, "Hard": 12]
+    let eggTimes = ["Soft": 3, "Medium": 4, "Hard": 7]
+    var secondsRemaing = 60
     
+    var timer = Timer()
+    
+    
+    @IBOutlet weak var askTime: UILabel!
+
     @IBAction func hardnessSelected(_ sender: UIButton) {
         
+        timer.invalidate()
+        
         let hardness = sender.currentTitle!
-        let result = eggTimes[hardness]!
+        secondsRemaing = eggTimes[hardness]!
+
         
-        print(result)
-        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
     }
-    
-    
-    
-    
 
+    
+    
+    @objc func updateCounter() {
+        //example functionality
+        if secondsRemaing > 0 {
+            print("\(secondsRemaing) seconds to the end of the world")
+            secondsRemaing -= 1
+        } else {
+            askTime.text = "Completed"
+        }
 
+    }
 }
+
 
