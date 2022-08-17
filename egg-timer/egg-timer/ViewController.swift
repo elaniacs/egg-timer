73309dc8bd7bafc56785cc23486821a9a43553a7
elaniacs <carensousaesilva@gmail.com>
2022-08-17 11:54:09 -0300

    barProgress implement

--- egg-timer/egg-timer/ViewController.swift
+++ egg-timer/egg-timer/ViewController.swift
@@ -10,32 +10,34 @@ import UIKit
 class ViewController: UIViewController {
 
     let eggTimes = ["Soft": 3, "Medium": 4, "Hard": 7]
-    var secondsRemaing = 60
-    
     var timer = Timer()
+    var totalTime = 0
+    var secondsPassed = 0
     
-    
+    @IBOutlet weak var barProgress: UIProgressView!
     @IBOutlet weak var askTime: UILabel!
 
     @IBAction func hardnessSelected(_ sender: UIButton) {
         
         timer.invalidate()
-        
         let hardness = sender.currentTitle!
-        secondsRemaing = eggTimes[hardness]!
-
+        totalTime = eggTimes[hardness]!
         
+        barProgress.progress = 0.0
+        secondsPassed = 0
+        askTime.text = hardness
+
         timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
     }
 
-    
-    
     @objc func updateCounter() {
-        //example functionality
-        if secondsRemaing > 0 {
-            print("\(secondsRemaing) seconds to the end of the world")
-            secondsRemaing -= 1
+        if secondsPassed < totalTime {
+            
+            secondsPassed += 1
+            barProgress.progress = Float(secondsPassed) / Float(totalTime)
+
         } else {
+            timer.invalidate()
             askTime.text = "Completed"
         }
 
